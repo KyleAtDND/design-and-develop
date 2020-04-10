d05d2eb4111a03d3842cc8e26241ea549e32b496
KyleAtDND <Wesley@DesignAndDevelop.io>
2020-04-10 11:39:50 -0500

    Updated theme basics

--- functions.php
+++ functions.php
@@ -18,34 +18,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 //-----------------------------------------------------------------------------------
 
   // Include extra Core functions
-<<<<<<< HEAD
   include 'includes/library.php';
 
   // Localhost Features
   if ( is_local_custom() ) {
     include 'includes/localize.php';
   }
-=======
-  include 'inc/library.php';
 
-  // Localhost Features
-  if ( is_local_custom() ) {
-    include 'inc/local/local-all.php';
-
-    if ( is_mac_custom() ) {
-      include 'inc/local/local-cond.php';
-    }
-  }
-
-  // Image functions
-  include 'inc/images.php';
-
-  // Shortcodes
-  include 'inc/shortcodes.php';
-
-  // Plugins
-  include 'inc/plugins/plugins.php';
-
-  // Content
-  include 'inc/content.php';
->>>>>>> b15933d51cb9ff86b812384260b6299fe7c38ecd
