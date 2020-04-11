ee0e64e32d7bd16916482bb3881016d2f664cbe1
KyleAtDND <Wesley@DesignAndDevelop.io>
2020-04-11 15:41:47 -0500

    Add clear github cache

--- includes/library.php
+++ includes/library.php
@@ -507,3 +507,19 @@ if ( ! defined( 'ABSPATH' ) ) {
     wp_enqueue_style( 'design-and-develop-style', get_stylesheet_uri() );
     //wp_add_inline_style( 'design-and-develop-style', $css );
   }, 0 );
+
+//-----------------------------------------------------------------------------------
+//  Refresh Styles
+//-----------------------------------------------------------------------------------
+
+  function dnd_reset_github_cache() {
+    global $wpdb;
+
+    $table         = is_multisite() ? $wpdb->base_prefix . 'sitemeta' : $wpdb->base_prefix . 'options';
+    $column        = is_multisite() ? 'meta_key' : 'option_name';
+    $delete_string = 'DELETE FROM ' . $table . ' WHERE ' . $column . ' LIKE %s LIMIT 1000';
+
+    $wpdb->query( $wpdb->prepare( $delete_string, [ '%ghu-%' ] ) ); // phpcs:ignore
+
+    return true;
+  }
